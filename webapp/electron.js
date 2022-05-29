b0470c16d832bc9ddb8ac1185da211c1618a600e
fynnfluegge <fynnfluegge@gmx.de>
2022-05-29 12:18:26 +0200

    add electron packager and mac-dmg-installer

--- webapp/electron.js
+++ webapp/electron.js
@@ -1,39 +1,39 @@
 const {app, BrowserWindow} = require('electron')
-    const url = require("url");
-    const path = require("path");
+  const url = require("url");
+  const path = require("path");
 
-    let mainWindow
+  let mainWindow
 
-    function createWindow () {
-      mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true
-        }
-      })
-
-      mainWindow.loadURL(
-        url.format({
-          pathname: path.join(__dirname, `/build/index.html`),
-          protocol: "file:",
-          slashes: true
-        })
-      );
-      // Open the DevTools.
-      mainWindow.webContents.openDevTools()
-
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
-    }
-
-    app.on('ready', createWindow)
-
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') app.quit()
+  function createWindow () {
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
     })
 
-    app.on('activate', function () {
-      if (mainWindow === null) createWindow()
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, `/build/index.html`),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.on('closed', function () {
+      mainWindow = null
     })
+  }
+
+  app.on('ready', createWindow)
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+  })
+
+  app.on('activate', function () {
+    if (mainWindow === null) createWindow()
+  })
