d7535002992be1b9e868d3789a069998aa42d489
fynnfluegge <fynnfluegge@gmx.de>
2022-05-29 12:50:12 +0200

    Readme.md changes

--- webapp/electron.js
+++ webapp/electron.js
@@ -20,8 +20,6 @@ const {app, BrowserWindow} = require('electron')
         slashes: true
       })
     );
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools()
 
     mainWindow.on('closed', function () {
       mainWindow = null
