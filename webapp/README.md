375b2d13ac0506151a2e182add08c60120bacf84
fynnfluegge <fynnfluegge@gmx.de>
2022-06-19 22:23:50 +0200

    electron-builder added

--- webapp/README.md
+++ webapp/README.md
@@ -15,6 +15,7 @@ npm start
 ng build --configuration production
 ```
 
+## electron-packager
 ### Package Electron App
 ```
 ./node_modules/.bin/electron-packager . --overwrite --platform=darwin --arch=x64 --icon=./src/assets/icons/mac/icon.icns --prune=true --out=release-builds --ignore="node_modules*|src*" rocketnotes
@@ -22,5 +23,10 @@ ng build --configuration production
 
 ### Create Installer
 ```
-./node_modules/.bin/electron-installer-dmg ./release-builds/rocketnotes-darwin-x64/rocketnotes.app rocketnote
+./node_modules/.bin/electron-installer-dmg ./release-builds/rocketnotes-darwin-x64/rocketnotes.app rocketnotes
+```
+## electron-builder
+### electron builder build and publish
+```
+./node_modules/.bin/electron-builder --publish always
 ```
--- webapp/electron.js
+++ webapp/electron.js
@@ -1,38 +1,113 @@
-const {app, BrowserWindow} = require('electron')
-  const url = require("url");
-  const path = require("path");
+const {app, dialog, Menu, BrowserWindow} = require('electron')
+const {autoUpdater} = require("electron-updater");
+const log = require('electron-log');
+const url = require("url")
+const path = require("path")
+const isMac = process.platform === 'darwin'
 
-  let mainWindow
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
-  function createWindow () {
-    mainWindow = new BrowserWindow({
-      // titleBarStyle: 'hidden',
-      width: 1024,
-      height: 800,
-      webPreferences: {
-        nodeIntegration: true
-      }
+let template = []
+if (isMac) {
+    // OS X
+    const name = app.getName();
+    template.unshift({
+        label: name,
+        submenu: [
+          {
+              label: 'About ' + name,
+              role: 'about'
+          },
+          { type: 'separator' },
+          {
+            label: 'Check for updates', 
+            enabled: true, 
+            click() { autoUpdater.checkForUpdatesAndNotify() }
+          },
+          { type: 'separator' },
+          {
+              role: 'quit',
+              label: 'Quit',
+              accelerator: 'Command+Q'
+          },
+        ]
     })
+}
 
-    mainWindow.loadURL(
-      url.format({
-        pathname: path.join(__dirname, `/build/index.html`),
-        protocol: "file:",
-        slashes: true
-      })
-    );
+let mainWindow
 
-    mainWindow.on('closed', function () {
-      mainWindow = null
-    })
-  }
-
-  app.on('ready', createWindow)
-
-  app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    // titleBarStyle: 'hidden',
+    width: 1024,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
 
-  app.on('activate', function () {
-    if (mainWindow === null) createWindow()
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/build/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
   })
+}
+
+app.on('ready', () => {
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  createWindow()
+})
+
+// app.on('ready', () => {
+//   // autoUpdater.checkForUpdatesAndNotify()
+// });
+
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') app.quit()
+// })
+
+// app.on('activate', function () {
+//   if (mainWindow === null) createWindow()
+// })
+
+// autoUpdater.on('checking-for-update', () => {
+//   sendStatusToWindow('Checking for update...');
+// });
+
+// autoUpdater.on('update-available', (info) => {
+//   sendStatusToWindow('Update available.');
+// });
+
+// autoUpdater.on('update-not-available', (info) => {
+//   sendStatusToWindow('Update not available.');
+// });
+
+// autoUpdater.on('error', (err) => {
+//   sendStatusToWindow('Error in auto-updater. ' + err);
+// });
+
+// autoUpdater.on('download-progress', (progressObj) => {
+//   let log_message = "Download speed: " + progressObj.bytesPerSecond;
+//   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+//   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+//   sendStatusToWindow(log_message);
+// });
+
+// autoUpdater.on('update-downloaded', (info) => {
+//   sendStatusToWindow('Update downloaded');
+// });
+
+// const sendStatusToWindow = function(text) {
+//   mainWindow.webContents.send('message', text);
+// }
