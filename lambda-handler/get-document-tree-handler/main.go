10e85a0876721da1f359cce99e7a6edbcad69406
fynnfluegge <fynnfluegge@gmx.de>
2023-06-10 20:30:02 +0200

    add access controll allow methods header

--- lambda-handler/get-document-tree-handler/main.go
+++ lambda-handler/get-document-tree-handler/main.go
@@ -84,6 +84,7 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
+			"Access-Control-Allow-Methods":     "GET,POST,OPTIONS",
 			"Access-Control-Allow-Origin":      "*", // Required for CORS support to work locally
 			"Access-Control-Allow-Credentials": "true",
 		},
