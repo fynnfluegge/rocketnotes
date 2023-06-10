1f8e39480e2262abd50888df940b59d43837ea22
fynnfluegge <fynnfluegge@gmx.de>
2023-06-10 21:14:55 +0200

    test

--- lambda-handler/get-document-tree-handler/main.go
+++ lambda-handler/get-document-tree-handler/main.go
@@ -84,9 +84,9 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
-			"Access-Control-Allow-Methods":     "GET,POST,OPTIONS",
-			"Access-Control-Allow-Origin":      "*", // Required for CORS support to work locally
-			"Access-Control-Allow-Headers":     "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+			// "Access-Control-Allow-Methods":     "GET,POST,OPTIONS",
+			"Access-Control-Allow-Origin": "*", // Required for CORS support to work locally
+			// "Access-Control-Allow-Headers":     "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
 			"Access-Control-Allow-Credentials": "true",
 		},
 		Body: string(b),
