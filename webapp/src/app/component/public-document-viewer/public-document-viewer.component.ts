be9ee1ca3af94ce1dd25b11fb0a66b8b20f22867
Fynn Flügge <fynnfluegge@gmx.de>
2023-06-23 19:36:59 +0200

    chore(webapp): apply prettier format (#68)

--- webapp/src/app/component/public-document-viewer/public-document-viewer.component.ts
+++ webapp/src/app/component/public-document-viewer/public-document-viewer.component.ts
@@ -6,28 +6,30 @@ import { BasicRestService } from 'src/app/service/basic-rest.service';
 @Component({
   selector: 'app-public-document-viewer',
   templateUrl: './public-document-viewer.component.html',
-  styleUrls: ['./public-document-viewer.component.scss']
+  styleUrls: ['./public-document-viewer.component.scss'],
 })
 export class PublicDocumentViewerComponent implements OnInit {
-
   private id: string;
   public title: string;
   public content: string;
 
-  constructor(private basicRestService : BasicRestService, private route: ActivatedRoute, private titleService: Title) { }
+  constructor(
+    private basicRestService: BasicRestService,
+    private route: ActivatedRoute,
+    private titleService: Title
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => { 
+    this.route.paramMap.subscribe((params) => {
       this.id = params.get('id');
       if (this.id) {
-        this.basicRestService.get("shared/" + this.id).subscribe(message => {
-          var document = JSON.parse(JSON.stringify(message))
-          this.content = document.content
-          this.title = document.title
+        this.basicRestService.get('shared/' + this.id).subscribe((message) => {
+          var document = JSON.parse(JSON.stringify(message));
+          this.content = document.content;
+          this.title = document.title;
           this.titleService.setTitle(document.title);
         });
       }
     });
   }
-
 }
