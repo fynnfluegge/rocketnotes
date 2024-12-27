7501510a13c0bc4570f53e93616a763b0f26c24a
fynnfluegge <fynnfluegge@gmx.de>
2024-12-27 11:35:12 +0100

    fix test

--- webapp/src/app/component/dialog-config/config-dialog.component.spec.ts
+++ webapp/src/app/component/dialog-config/config-dialog.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfigDialogComponent } from './config-dialog.component';
+import { BasicRestService } from 'src/app/service/basic-rest.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 describe('ConfigDialogComponent', () => {
   let component: ConfigDialogComponent;
@@ -12,13 +15,18 @@ describe('ConfigDialogComponent', () => {
     }).compileComponents();
   });
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ConfigDialogComponent],
+      imports: [HttpClientModule, FormsModule],
+      providers: [BasicRestService],
+    }).compileComponents();
     fixture = TestBed.createComponent(ConfigDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
