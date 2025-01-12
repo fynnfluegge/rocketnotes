5fbe80e66399af3e44fe431e61f75ad1011d8f04
fynnfluegge <fynnfluegge@gmx.de>
2025-01-12 18:56:42 +0100

    delete vectors on document deletion

--- lambda-handler/delete-document-handler/main.go
+++ lambda-handler/delete-document-handler/main.go
@@ -15,21 +15,26 @@ import (
 )
 
 type Document struct {
-	ID           string `json:"id"`
-	ParentId     string `json:"parentId"`
-	UserId       string `json:"userId"`
-	Title        string `json:"title"`
-	Content      string `json:"content"`
+	ID           string    `json:"id"`
+	ParentId     string    `json:"parentId"`
+	UserId       string    `json:"userId"`
+	Title        string    `json:"title"`
+	Content      string    `json:"content"`
 	LastModified time.Time `json:"lastModified"`
-	Deleted      bool   `json:"deleted"`
-	IsPublic     bool   `json:"isPublic"`
+	Deleted      bool      `json:"deleted"`
+	IsPublic     bool      `json:"isPublic"`
+}
+
+type SqsMessage struct {
+	UserId        string `json:"userId"`
+	DocumentId    string `json:"documentId"`
+	DeleteVectors bool   `json:"deleteVectors"`
 }
 
 func init() {
 }
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
 	id := request.PathParameters["documentId"]
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
@@ -80,6 +85,23 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	_, err = svc.PutItem(input)
 	if err != nil {
 		log.Fatalf("Got error calling PutItem: %s", err)
+	}
+
+	if user_config.Item != nil && os.Getenv("USE_LOCAL_DYNAMODB") != "1" {
+		log.Printf("Recreating index for document %s", item.Body.Document.ID)
+		qsvc := sqs.New(sess)
+
+		m := SqsMessage{item.Body.Document.UserId, item.Body.Document.ID, true}
+		b, err := json.Marshal(m)
+
+		_, err = qsvc.SendMessage(&sqs.SendMessageInput{
+			DelaySeconds: aws.Int64(0),
+			MessageBody:  aws.String(string(b)),
+			QueueUrl:     aws.String(os.Getenv("queueUrl")),
+		})
+		if err != nil {
+			log.Fatalf("Error sending sqs message: %s", err)
+		}
 	}
 
 	return events.APIGatewayProxyResponse{
